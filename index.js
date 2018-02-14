93195df08cb8ccfe0f340f398488f6e6d9259605
stefan50 <ste.angelov.tues19@gmail.com>
2018-02-14 12:13:47 +0200

    Style

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ function myIndex()
 	function waitText()
 	{
 		var parag = document.createElement("p");
-		parag.style.color = "white";
-		var text = document.createTextNode("Did you like the video?");
+		parag.style.color = "red";
+		parag.style.textAlign = "center";
+		parag.style.textShadow = "3px 2px green";
+		parag.style.font = "italic bold 40px Georgia, serif";
+		var text = document.createTextNode("Did you like the video? Here are some more:");
 		parag.appendChild(text);
 		var body = document.getElementsByTagName("body")[0];
 		body.appendChild(parag); 
 	}
-
 	
 	setTimeout(function() {
 				waitText();
