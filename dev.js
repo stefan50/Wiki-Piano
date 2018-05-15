be56a82dbb5b5714ca3f0e3968ff3e22b79488ec
stefan50 <ste.angelov.tues19@gmail.com>
2018-05-15 09:47:43 +0300

    Minor changes

--- dev.js
+++ dev.js
@@ -7,8 +7,21 @@ function myDev()
 										{
 											par.remove();
 											alert("Very well! You found the unneeded text! Such observation skills will definitely come in handy when playing the game!");
-										});		
-	} 
+										});
+	}
 
+	/*function showTable()
+	{
+		let table = document.createElement("table");
+		let tr = document.createElement("tr");
+		for(int i=0; i<3; i++)
+		{
+			let th = document.createElement("th");
+			th.value = "sth";
+			tr.appendChild(th);
+		}
+
+		table.appendChild(tr);
+	}*/
 	pGoesOut();
 }
